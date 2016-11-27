9a83e63b88c720cd94080609473e2467377677cb
Hanson Li <lihsca@gmail.com>
2016-11-26 22:17:06 -0500

    config.php

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+return [ 
+    'StoreNameA' => [
+        'AccessKey'     => '',
+        'SellerId'      => '',
+        'MarketplaceId' => '',
+        'SecretKey'     => '',
+        'ServiceUrl'    => 'https://mws.amazonservices.ca/',
+        'AuthToken'     => '',
+    ],
+
+    'StoreNameB' => [
+        'AccessKey'     => '',
+        'SellerId'      => '',
+        'MarketplaceId' => '',
+        'SecretKey'     => '',
+        'ServiceUrl'    => 'https://mws.amazonservices.com/',
+        'AuthToken'     => '',
+    ]
+];
