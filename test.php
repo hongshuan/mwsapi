4391843c1682d3375b050fcccd08f51bd389ee3c
Hanson Li <lihsca@gmail.com>
2016-11-27 19:15:14 -0500

    empty tests for all apis

--- test.php
+++ test.php
@@ -3,25 +3,131 @@
 include 'Client.php';
 
 $config = include 'config.php';
-$client = new Amazon\Mws\Client($config['StoreNameA']);
 
-$feedApi = $client->feedApi();
-$financeApi = $client->financeApi();
+function testFeedApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $feedApi = $client->feedApi();
+    $feedApi->getServiceStatus();
+}
+testFeedApi($config);
 
-$fbaInboundApi = $client->fbaInboundApi();
-$fbaOutboundApi = $client->fbaOutboundApi();
-$fbaInventoryApi = $client->fbaInventoryApi();
+function testFinanceApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $financeApi = $client->financeApi();
+    $financeApi->getServiceStatus();
+}
+#testFinanceApi($config);
 
-$merchantFulfillmentApi = $client->merchantFulfillmentApi();
+function testFbaInboundApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $fbaInboundApi = $client->fbaInboundApi();
+    $fbaInboundApi->getServiceStatus();
+}
+#testFbaInboundApi($config);
 
-$offAmazonPaymentApi = $client->offAmazonPaymentApi();
-$offAmazonPaymentsSandboxApi = $client->offAmazonPaymentsSandboxApi();
+function testFbaOutboundApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $fbaOutboundApi = $client->fbaOutboundApi();
+    $fbaOutboundApi->getServiceStatus();
+}
+#testFbaOutboundApi($config);
 
-$orderApi = $client->orderApi();
-$productApi = $client->productApi();
-$recommendationApi = $client->recommendationApi();
-$reportApi = $client->reportApi();
-$reportScheduleApi = $client->reportScheduleApi();
-$sellerApi = $client->sellerApi();
-$subscriptionApi = $client->subscriptionApi();
-$subscriptionDestinationApi = $client->subscriptionDestinationApi();
+function testFbaInventoryApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $fbaInventoryApi = $client->fbaInventoryApi();
+    $fbaInventoryApi->getServiceStatus();
+}
+#testFbaInventoryApi($config);
+
+function testMerchantFulfillmentApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $merchantFulfillmentApi = $client->merchantFulfillmentApi();
+    $merchantFulfillmentApi->getServiceStatus();
+}
+#testMerchantFulfillmentApi($config);
+
+function testOffAmazonPaymentApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $offAmazonPaymentApi = $client->offAmazonPaymentApi();
+    $offAmazonPaymentApi->getServiceStatus();
+}
+#testOffAmazonPaymentApi($config);
+
+function testOffAmazonPaymentsSandboxApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $offAmazonPaymentsSandboxApi = $client->offAmazonPaymentsSandboxApi();
+    $offAmazonPaymentsSandboxApi->getServiceStatus();
+}
+#testOffAmazonPaymentsSandboxApi($config);
+
+function testOrderApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $orderApi = $client->orderApi();
+    $orderApi->getServiceStatus();
+}
+#testOrderApi($config);
+
+function testProductApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $productApi = $client->productApi();
+    $productApi->getServiceStatus();
+}
+#testProductApi($config);
+
+function testRecommendationApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $recommendationApi = $client->recommendationApi();
+    $recommendationApi->getServiceStatus();
+}
+#testRecommendationApi($config);
+
+function testReportApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $reportApi = $client->reportApi();
+    $reportApi->getServiceStatus();
+}
+#testReportApi($config);
+
+function testReportScheduleApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $reportScheduleApi = $client->reportScheduleApi();
+    $reportScheduleApi->getServiceStatus();
+}
+#testReportScheduleApi($config);
+
+function testSellerApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $sellerApi = $client->sellerApi();
+    $sellerApi->getServiceStatus();
+}
+#testSellerApi($config);
+
+function testSubscriptionApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $subscriptionApi = $client->subscriptionApi();
+    $subscriptionApi->getServiceStatus();
+}
+#testSubscriptionApi($config);
+
+function testSubscriptionDestinationApi($config)
+{
+    $client = new Amazon\Mws\Client($config['StoreNameA']);
+    $subscriptionDestinationApi = $client->subscriptionDestinationApi();
+    $subscriptionDestinationApi->getServiceStatus();
+}
+#testSubscriptionDestinationApi($config);
