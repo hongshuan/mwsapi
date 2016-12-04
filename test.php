fc7eac95a450c8cc4fd89baf1c409ac458e8b02a
Hanson Li <lihsca@gmail.com>
2016-12-04 12:35:55 -0500

    testing change

--- test.php
+++ test.php
@@ -38,82 +38,85 @@ function testFeedApi($config)
 }
 #testFeedApi($config);
 
-function testFinanceApi($config)
-{
-    $client = new Amazon\Mws\Client($config);
-    $financeApi = $client->financeApi();
-
-#   $response = $financeApi->getServiceStatus();
-#   $response = $financeApi->listFinancialEventGroups();  // argument required
-
-    print_r($response);
-}
+#function testFinanceApi($config)
+#{
+#    $client = new Amazon\Mws\Client($config);
+#    $financeApi = $client->financeApi();
+#
+##   $response = $financeApi->getServiceStatus();
+##   $response = $financeApi->listFinancialEventGroups();  // argument required
+#
+#    print_r($response);
+#}
 #testFinanceApi($config);
 
-function testFbaInboundApi($config)
-{
-    $client = new Amazon\Mws\Client($config);
-#   $fbaInboundApi = $client->fbaInboundApi();
-    $fbaInboundApi = $client->fulfillmentInboundApi();
-    $response = $fbaInboundApi->getServiceStatus();
-    print_r($response);
-}
-#testFbaInboundApi($config);
-
-function testFbaOutboundApi($config)
-{
-    $client = new Amazon\Mws\Client($config);
-#   $fbaOutboundApi = $client->fbaOutboundApi();
-    $fbaOutboundApi = $client->fulfillmentOutboundApi();
-    $response = $fbaOutboundApi->getServiceStatus();
-    print_r($response);
-}
-#testFbaOutboundApi($config);
-
-function testFbaInventoryApi($config)
-{
-    $client = new Amazon\Mws\Client($config);
-#   $fbaInventoryApi = $client->fbaInventoryApi();
-    $fbaInventoryApi = $client->fulfillmentInventoryApi();
-    $response = $fbaInventoryApi->getServiceStatus();
-    print_r($response);
-}
-#testFbaInventoryApi($config);
-
-function testMerchantFulfillmentApi($config)
-{
-    $client = new Amazon\Mws\Client($config);
-    $merchantFulfillmentApi = $client->merchantFulfillmentApi();
-    $response = $merchantFulfillmentApi->getServiceStatus();
-    print_r($response);
-}
-#testMerchantFulfillmentApi($config);
-
-function testOffAmazonPaymentApi($config)
-{
-    $client = new Amazon\Mws\Client($config);
-    $offAmazonPaymentApi = $client->offAmazonPaymentApi();
-    $response = $offAmazonPaymentApi->getServiceStatus();
-    print_r($response);
-}
-#testOffAmazonPaymentApi($config);
-
-function testOffAmazonPaymentsSandboxApi($config)
-{
-    $client = new Amazon\Mws\Client($config);
-    $offAmazonPaymentsSandboxApi = $client->offAmazonPaymentsSandboxApi();
-    $response = $offAmazonPaymentsSandboxApi->getServiceStatus();
-    print_r($response);
-}
-#testOffAmazonPaymentsSandboxApi($config);
+#function testFbaInboundApi($config)
+#{
+#    $client = new Amazon\Mws\Client($config);
+##   $fbaInboundApi = $client->fbaInboundApi();
+#    $fbaInboundApi = $client->fulfillmentInboundApi();
+#    $response = $fbaInboundApi->getServiceStatus();
+#    print_r($response);
+#}
+##testFbaInboundApi($config);
+#
+#function testFbaOutboundApi($config)
+#{
+#    $client = new Amazon\Mws\Client($config);
+##   $fbaOutboundApi = $client->fbaOutboundApi();
+#    $fbaOutboundApi = $client->fulfillmentOutboundApi();
+#    $response = $fbaOutboundApi->getServiceStatus();
+#    print_r($response);
+#}
+##testFbaOutboundApi($config);
+#
+#function testFbaInventoryApi($config)
+#{
+#    $client = new Amazon\Mws\Client($config);
+##   $fbaInventoryApi = $client->fbaInventoryApi();
+#    $fbaInventoryApi = $client->fulfillmentInventoryApi();
+#    $response = $fbaInventoryApi->getServiceStatus();
+#    print_r($response);
+#}
+##testFbaInventoryApi($config);
+#
+#function testMerchantFulfillmentApi($config)
+#{
+#    $client = new Amazon\Mws\Client($config);
+#    $merchantFulfillmentApi = $client->merchantFulfillmentApi();
+#    $response = $merchantFulfillmentApi->getServiceStatus();
+#    print_r($response);
+#}
+##testMerchantFulfillmentApi($config);
+#
+#function testOffAmazonPaymentApi($config)
+#{
+#    $client = new Amazon\Mws\Client($config);
+#    $offAmazonPaymentApi = $client->offAmazonPaymentApi();
+#    $response = $offAmazonPaymentApi->getServiceStatus();
+#    print_r($response);
+#}
+##testOffAmazonPaymentApi($config);
+#
+#function testOffAmazonPaymentsSandboxApi($config)
+#{
+#    $client = new Amazon\Mws\Client($config);
+#    $offAmazonPaymentsSandboxApi = $client->offAmazonPaymentsSandboxApi();
+#    $response = $offAmazonPaymentsSandboxApi->getServiceStatus();
+#    print_r($response);
+#}
+##testOffAmazonPaymentsSandboxApi($config);
 
 function testOrderApi($config)
 {
     $client = new Amazon\Mws\Client($config);
     $orderApi = $client->orderApi();
+
 #   $response = $orderApi->getServiceStatus();
 #   $response = $orderApi->getOrder(['701-2909100-8898624', '701-7599957-6137843']);
+
     $response = $orderApi->listOrderItems('701-2909100-8898624');
+
     print_r($response);
 }
 #testOrderApi($config);
